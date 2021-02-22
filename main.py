2949c4dbcc5df5afa6e1b70b6f4134a20857f614
Laurin Reimann <laurin@laurinr.xyz>
2021-02-22 18:03:25 +0100

    + beginnings of parseWgConf.py + second lines of code in main.py

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 #! python3
 # -*- coding: utf-8 -*-
 
-import os
+import os, configparser
 
 
 class Wireguard:
 
+    class Peer:
+        def __init__(self, peerName, publicKey, allowedIPs):
+            self.peerName = peerName
+            self.publicKey = publicKey
+            self.allowedIPs = allowedIPs
+
+    peers = []
+
     def __init__(self, interfaceName: str, configFile: str):
         self.interfaceName = interfaceName
         self.configFile = configFile
+        self.config = configparser.ConfigParser()
+        self.config.read(configFile)
 
-    def addPeer(self, peerName="", publicKey="", allowedIPs=None):
-        self.allowedIPs = [] if not allowedIPs else allowedIPs
+    def addPeer(self, publicKey: str, peerName: str = None, allowedIPs: list = None):
+        allowedIPs = [] if not allowedIPs else allowedIPs
+        peerName = publicKey if not allowedIPs else peerName
+        self.peers.append(self.Peer(peerName, publicKey, allowedIPs))
 
 
 if __name__ == '__main__':
-    wg = Wireguard()
+    wg = Wireguard("wgtest", u"test/test.conf")
